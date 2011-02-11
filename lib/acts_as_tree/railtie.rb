80944bdb4c60783380c1eff6f39b3bc3fae5dfd6
newbray <chris_guy_84@hotmail.com>
2011-02-11 08:39:54 -0800

    test

--- lib/acts_as_tree/railtie.rb
+++ lib/acts_as_tree/railtie.rb
@@ -7,7 +7,7 @@ require 'acts_as_tree/helper'
       class Railtie < ::Rails::Railtie
         config.before_initialize do
           ActiveSupport.on_load :action_view do
-            ActionView::Base.send(:include, Acts::Tree::Helper)
+            ActionView::Base.send(:include, ActiveRecord::Acts::Tree::Helper)
           end
         end
       end
